e545291a19d7f293ac7b8858715527cc71c0b68c
Fatima Farah <fatima.farah@connect-group.com>
2020-07-20 15:32:46 +0100

    Split Array and List practice task.

--- src/main/java/Practice/ArraysPractice.java
+++ src/main/java/Practice/ArraysPractice.java
@@ -1,8 +1,5 @@
 package Practice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ArraysPractice {
 
   public static void main(String[] args) {
@@ -15,25 +12,5 @@ public class ArraysPractice {
 
     }
 
-    //List
-
-    List<Integer> listTest = new ArrayList();
-
-    listTest.add(1);
-    listTest.add(2);
-    listTest.add(3);
-    listTest.add(4);
-    listTest.add(5);
-    listTest.add(6);
-    listTest.add(7);
-    listTest.add(8);
-    listTest.add(9);
-
-    for (int i = 0; i < 4; i++) {
-
-      System.out.println((i));
-    }
-
   }
-
 }
