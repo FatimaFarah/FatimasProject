3b896a688f368ff4404ab2d503e3caa505548d42
Fatima Farah <fatima.farah@connect-group.com>
2020-07-21 14:08:31 +0100

    Adding values to three seperate lists - darts one, two and three,

---
+++ src/main/java/Practice/Darts.java
@@ -0,0 +1,47 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Darts {
+  public Darts() {
+
+    List<String> dartsOne = new ArrayList<>();
+
+    dartsOne.add("D1");
+    dartsOne.add("D3");
+    dartsOne.add("S2");
+    dartsOne.add("D2");
+    dartsOne.add("S4");
+    dartsOne.add("S1");
+    dartsOne.add("S1");
+    dartsOne.add("S1");
+    dartsOne.add("D1");
+    dartsOne.add("D1");
+    dartsOne.add("S2");
+
+    List<String> dartsTwo = new ArrayList<>();
+
+    dartsTwo.add("D2");
+    dartsTwo.add("D2");
+    dartsTwo.add("D1");
+    dartsTwo.add("D1");
+    dartsTwo.add("S1");
+    dartsTwo.add("T1");
+    dartsTwo.add("S3");
+    dartsTwo.add("D1");
+    dartsTwo.add("S2");
+    dartsTwo.add("S2");
+
+    List<String> dartsThree = new ArrayList<>();
+
+    dartsThree.add("D2");
+    dartsThree.add("D1");
+    dartsThree.add("D1");
+    dartsThree.add("D1");
+    dartsThree.add("D1");
+    dartsThree.add("D1");
+
+
+  }
+}
