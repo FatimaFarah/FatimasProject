1187585b9794c00330465a4cc7da0734050d7872
Alex Porter <alex.porter@connect-group.com>
2020-07-29 14:12:13 +0100

    darts hints

--- src/main/java/Practice/Darts.java
+++ src/main/java/Practice/Darts.java
@@ -1,47 +1,42 @@
 package Practice;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Darts {
-  public Darts() {
 
-    List<String> dartsOne = new ArrayList<>();
-
-    dartsOne.add("D1");
-    dartsOne.add("D3");
-    dartsOne.add("S2");
-    dartsOne.add("D2");
-    dartsOne.add("S4");
-    dartsOne.add("S1");
-    dartsOne.add("S1");
-    dartsOne.add("S1");
-    dartsOne.add("D1");
-    dartsOne.add("D1");
-    dartsOne.add("S2");
-
-    List<String> dartsTwo = new ArrayList<>();
-
-    dartsTwo.add("D2");
-    dartsTwo.add("D2");
-    dartsTwo.add("D1");
-    dartsTwo.add("D1");
-    dartsTwo.add("S1");
-    dartsTwo.add("T1");
-    dartsTwo.add("S3");
-    dartsTwo.add("D1");
-    dartsTwo.add("S2");
-    dartsTwo.add("S2");
-
-    List<String> dartsThree = new ArrayList<>();
-
-    dartsThree.add("D2");
-    dartsThree.add("D1");
-    dartsThree.add("D1");
-    dartsThree.add("D1");
-    dartsThree.add("D1");
-    dartsThree.add("D1");
-
+  /*Todo:: Think about how you would like to store this information.
+  This method will return a list but each entry will have 3 numbers ie {first dart, second dart, third dart}.
+  Left the return type as void for now just so it compiles.
+   */
+  private void getPossibleCombinationsOf3Darts() {
 
   }
+
+  private List<Integer> getPossibleScoresWith1Dart() {
+    List<Integer> scores = new ArrayList<>();
+
+    //Todo:: Loop to add numbers 1-20 their double and their triples.
+
+    //Leave 25 and 50 outside of loop as they dont match the pattern of the rest of the numbers
+    scores.add(25);
+    scores.add(50);
+    return scores;
+  }
+
+  @Test
+  public void possibleScoresWith1DartTest() {
+    List<Integer> actual = getPossibleScoresWith1Dart();
+    List<Integer> expected = Arrays.asList(1,2,3,2,4,6,3,6,9,4,8,12,5,10,15,6,12,18,7,14,21,8,16,24,9,18,27,10,20,30,
+      11,22,33,12,24,36,13,26,39,14,28,42,15,30,45,15,32,48,17,34,51,18,36,54,19,38,57,20,40,60,25,50);
+    Collections.sort(actual);
+    Collections.sort(expected);
+
+    assertThat(actual, equalTo(expected));
+  }
+
 }
