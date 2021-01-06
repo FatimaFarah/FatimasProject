cd02727220acd065c15052d371671a309a54fe09
Fatima Farah <fatima.farah@connect-group.com>
2021-01-06 15:13:52 +0000

    Created test page for feminism page.

--- src/main/java/Pages/Feminism.java
+++ src/main/java/Pages/Feminism.java
@@ -8,12 +8,6 @@ public class Feminism extends BasePage {
   public Feminism(WebDriver driver) {
     super(driver);
   }
-    //Site title and description methods
-    public WebElement getSiteTitle(){
-    return getDriver().findElement(By.cssSelector(".site-title")); }
-    public WebElement getSiteDescription() {
-    return getDriver().findElement(By.cssSelector(".site-description")); }
-
     public WebElement getCategoryTextFeminism(){
     return getDriver().findElement(By.cssSelector(".page-title")); }
 
