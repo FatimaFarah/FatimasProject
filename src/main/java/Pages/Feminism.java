429249da81f9ef3fc2a7d1cb5ec0748386c0428d
Fatima Farah <fatima.farah@connect-group.com>
2021-01-07 11:58:13 +0000

    Created new methods and tests to loop through entry dates and hrefs.

--- src/main/java/Pages/Feminism.java
+++ src/main/java/Pages/Feminism.java
@@ -3,12 +3,33 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class Feminism extends BasePage {
   public Feminism(WebDriver driver) {
     super(driver);
   }
+
+  //Methods
     public WebElement getCategoryTextFeminism(){
     return getDriver().findElement(By.cssSelector(".page-title")); }
+
+    public List<WebElement> getAllPosts() {
+    return until(ExpectedConditions.visibilityOfAllElementsLocatedBy
+        (By.cssSelector(".post .entry-content")));
+    }
+
+    public WebElement getHrefFromPosts(WebElement post ) {
+    return post.findElement(By.cssSelector(".post .entry-content"));
+    }
+
+    public List<WebElement> getAllEntryDates() {
+    return until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector
+        (".entry-date published updated")));
+    }
+
+
 
   }
--- src/main/java/Tests/FeminismPageTest.java
+++ src/main/java/Tests/FeminismPageTest.java
@@ -4,6 +4,8 @@ import Pages.Feminism;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class FeminismPageTest extends BaseIT{
   public Feminism feminism = new Feminism(driver);
 
@@ -15,5 +17,12 @@ public class FeminismPageTest extends BaseIT{
   @Test
   public void printCategoryTest(){
     System.out.println(feminism.getCategoryTextFeminism().getText());
+  }
+
+  @Test
+  public void areHrefsDisplayed() {
+    feminism.getAllPosts().forEach(post -> {
+      assertNotNull(feminism.getHrefFromPosts(post).getAttribute("href"));
+    });
   }
 }
